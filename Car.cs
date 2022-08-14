324e23b38199dcc4f19fbb9177935ceda490d1a6
Shrikant Pandey <shrikantpandeydotnet@gmail.com>
2022-08-14 17:03:11 +0800

    Add a new car class.

---
+++ Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITExpertsERa_BatchMay2022
+{
+    class Car
+    {
+        public int CarName { get; set; }
+    }
+}
